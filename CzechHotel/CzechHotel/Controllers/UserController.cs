d325dcb376e7a14eca8ddc476993571371970f40
Nat-chan256 <Nat.Chan.Des@yandex.ru>
2021-03-02 21:00:04 +0300

    02.03.21
    - Создана диаграмма классов
    - Созданы классы UserModel и UserController

---
+++ CzechHotel/CzechHotel/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CzechHotel.Models;
+
+namespace CzechHotel.Controllers
+{
+    class UserController
+    {
+        public UserController()
+        { }
+
+        public void Save(UserModel _user)
+        { }
+
+        List<UserModel> GetUsersData()
+        {
+            return null;
+        }
+
+        void SetNewUserData(string _genderName, DateTime _birthDate,
+            int _passportSeries, string _phoneNumber, int _roomNumber,
+            bool _withChildren, int _amountOfResidents, DateTime _arrivalDate,
+            DateTime _departureDate)
+        { }
+
+        private class DBController
+        {
+            DBController()
+            { }
+        }
+    }
+}
---
+++ CzechHotel/CzechHotel/Models/UserModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CzechHotel.Models
+{
+    class UserModel
+    {
+        public UserModel(string _name, string _surname, int _passportSeries, bool _withChildren)
+        { }
+
+        public UserModel(string _name, string _surname, string _genderName,
+            DateTime _birthDate, int _passportSeries, int _passportNumber,
+            string _phoneNumber, bool _withChildren, int _amountOfResidents,
+            DateTime _arrivalDate, DateTime _departureDate)
+        { }
+
+        private class GenderModel
+        {
+            public GenderModel(string _name)
+            { }
+        }
+    }
+}
